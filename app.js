ff9e952a201751aa986e7ee25e6cf287adadf1a8
BodnarB <bodnarbog94@gmail.com>
2026-03-28 11:29:29 +0100

    up

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let finalText = document.querySelector('#final-text')
 let copyBtn = document.querySelector('.copy-btn')
 let resetBtn = document.querySelector('.reset-btn')
 
-let defaultText = `Xh (b) és az olasz Yg a Magyarország - Olaszország férfi kézilabda felkészülési mérkőzésen a Tatabányai Multifunkcionális Sportcsarnokban 2026. március 21-én. `
+let defaultText = `Xh, a Ferencváros (b) és Yg, a Borussia Dortmund játékosa a női kézilabda Bajnokok Ligája negyeddöntőbe jutásért játszott FTC-Rail Cargo Hungaria - Borussia Dortmund visszavágó mérkőzésen az Érd Arénában 2026. március 28-án. `
 
 inputText.value = defaultText
 
@@ -16,47 +16,36 @@ resetBtn.addEventListener('click', () => {
 
 let csapatok = {
     home: {
-        2: "Sipos Adrián",
-        7: "Bóka Bendegúz",
-        9: "Pergel Andrej",
-        11: "Ligetvári Patrik",
-        18: "Krakovszki Bence",
-        21: "Nagy Benedek",
-        23: "Máthé Dominik",
-        24: "Fazekas Gergő",
-        26: "Rodríguez Pedro",
-        31: "Szűcs Bence",
-        32: "Palasics Kristóf",
-        39: "Bodó Richárd",
-        43: "Ilic Zoran",
-        45: "Rosta Miklós",
-        49: "Sztraka Dániel",
-        61: "Bartucz László",
-        71: "Ónodi-Jánoskúti Máté",
-        72: "Lukács Péter",
-        77: "Hanusz Egon",
-        90: "Tóth Levente",
-        91: "Imre Bence"
+        1: "Laura Glauser",
+        11: "Daria Dmitrieva",
+        15: "Hársfalvi Júlia",
+        16: "Böde-Bíró Blanka",
+        18: "Mette Tranborg",
+        21: "Márton Gréta",
+        26: "Antje Angela Malestein",
+        38: "Simon Petra Anna",
+        41: "Balázs Bítia",
+        42: "Klujber Katrin Gitta",
+        49: "Szeibert Anna Rebeka",
+        51: "Vilde Mortensen Ingstad",
+        58: "Bordás Réka",
+        72: "Dragana Cvijic"
     },
     guest: {
-        2: "Leo Prantner",
-        3: "Nicola Fadanelli",
-        5: "Gianluca Dapiran",
-        6: "Davide Bulzamini",
-        7: "Filippom Angiolini",
-        8: "Giacomo Savini",
-        10: "Marco Mengon",
-        11: "Nicolo D Antino",
-        15: "Simone Mengon",
-        18: "Valentino Dello Vicario",
-        20: "Davide Pugliese",
-        27: "Gabriele Somma",
-        30: "Tommaso Romei",
-        32: "Andrea Parisini",
-        40: "Simon Sirot",
-        51: "Pau Panitti",
-        62: "Alex Belardinelli",
-        90: "Salah Riahi"
+        1: "Teodora Roncevic",
+        4: "Alina Grijseels",
+        7: "Szikora Melinda",
+        8: "Maraike Kusian",
+        9: "Lisa Antl",
+        17: "Alicia Langer",
+        21: "Kaja Kamp Nielsen",
+        22: "Déborah Lassource",
+        24: "Guro Berland Husebo",
+        29: "Emma Olsson",
+        31: "Kelly Vollebregt",
+        33: "Guro Nestaker",
+        44: "Lara Egeling",
+        66: "Dana Bleckmann"
     }
 }
 
